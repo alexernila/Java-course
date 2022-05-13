3063c75787a451d5fba41f453c75c3491feb2dcd
Alex Ernila <84703241+alexernila@users.noreply.github.com>
2022-05-13 13:07:22 +0300

    Add files via upload

---
+++ reaadingUserInputChallenge/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int counter = 0;
+        int sum = 0;
+
+        while(true) {
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+            if(isAnInt){
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+                if(counter==10){
+                    break;
+                }
+            } else{
+                System.out.println("Invalid number");
+            }
+
+            scanner.nextLine(); // handle end of line (enter key)
+        }
+        System.out.println("Sum equals " + sum);
+        scanner.close();
+
+    }
+}
